f1e5d6059b34c6a268c888826ac1f31d78d6367e
shubhs2060 <shubhamsrivastava2050>
2018-12-30 21:43:40 +0530

    add capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,12 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
 
+set :application, "wizard-app"
+set :repo_url, "git@github.com:shubhs2060/wizard_app.git"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rvm_ruby_version, '2.3.1'
+set :passenger_restart_with_touch, true
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,7 +21,14 @@
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-
+role :app, %w{deploy_user@<public-ip-of-your-ec2-instance>}
+role :web, %w{deploy_user@<public-ip-of-your-ec2-instance>}
+role :db,  %w{deploy_user@<public-ip-of-your-ec2-instance>}
+set :ssh_options, {
+   keys: %w(<absolute-path-to-your-ec2-key-pair-file>),
+   forward_agent: false,
+   auth_methods: %w(publickey password)
+ }
 
 # Configuration
 # =============
